c2913724e6da6145faac701470980898ab71bca6
Yuan Wang <wangjunnige@163.com>
2018-01-30 15:10:47 +0800

    init

---
+++ WJHelpSelf/WJTest.h
@@ -0,0 +1,15 @@
+//
+//  WJTest.h
+//  SDKDemo
+//
+//  Created by Yuan Wang on 2018/1/30.
+//  Copyright © 2018年 Yuan Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WJTest : NSObject
+
+- (void)eat;
+
+@end
---
+++ WJHelpSelf/WJTest.m
@@ -0,0 +1,18 @@
+//
+//  WJTest.m
+//  SDKDemo
+//
+//  Created by Yuan Wang on 2018/1/30.
+//  Copyright © 2018年 Yuan Wang. All rights reserved.
+//
+
+#import "WJTest.h"
+
+@implementation WJTest
+
+- (void)eat{
+    
+    NSLog(@"吃吃吃");
+}
+
+@end
